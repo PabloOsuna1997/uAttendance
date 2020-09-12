b34150579996bc6e4f865d1c0ab429e9b73b8910
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-09-12 11:43:48 -0600

    feat: login

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -3,22 +3,28 @@ var router = express.Router();
 const aws = require('aws-sdk')
 const connection = require('../config/connectionDB')
 
+
+aws.config.update(connection.aws_remote_comfig)
+const client = new aws.DynamoDB.DocumentClient();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(connection)
   res.send("home of the page")
 });
 
-/* GET home page. */
-router.get('/registrer', async (req, res) => {
-  aws.config.update(connection.aws_remote_comfig)
-  const client = new aws.DynamoDB.DocumentClient();
+/* POST registrer. */
+router.post('/registrer', async (req, res) => {
+  let contrasena = req.body.contrasena
+  let nombre = req.body.nombre
+  let foto = req.body.foto
+
   let params = {
     TableName: 'tabla-usuario-semi1-pro1',
     Item: {
-      "nombre": "test 5",
-      "contasena": "password",
-      "foto": "url de la foto de s3"
+      "nombre": nombre,
+      "contrasena": contrasena,
+      "foto": foto
     }
   }
   await client.put(params, function (err, data){
@@ -37,5 +43,29 @@ router.get('/registrer', async (req, res) => {
     }
   })
 })
+
+
+  /*GET login de usuario*/
+router.post('/login', async (req, res) => {
+  let nombre = req.body.nombre
+  let contrasena = req.body.contrasena
+
+  let params = {
+        Key: {
+          "nombre": nombre,
+          "contasena": contrasena
+        },
+        TableName: "tabla-usuario-semi1-pro1"
+  }
+
+  await client.get(params, function (err, data){
+    if(!err){
+      res.send(data)
+    }else{
+      res.send('no existe')
+    }
+  })
+})
+
 
 module.exports = router;
